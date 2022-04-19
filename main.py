bb69f532d5e52bc0556f1c1815104833208545e1
gastudillo <47458556+gabrielastudillo@users.noreply.github.com>
2022-04-19 17:42:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#
+# Copyright (c) 2019, Pycom Limited.
+
+from network import WLAN
+from mqtt import MQTTClient
+import machine
+import time
+import socket
+import ssl
+
+def settimeout(duration): 
+    pass
+
+PARAMS = {'keyfile': 'cert/client.key', 
+            'certfile': 'cert/client.csr', 
+            'cert_reqs': ssl.CERT_REQUIRED, 
+            'ca_certs': 'cert/client.crt'}
+
+wlan = WLAN(mode=WLAN.STA)
+#wlan.antenna(WLAN.EXT_ANT)
+wlan.connect("BELL094", auth=(WLAN.WPA2, "14FADA65171E"), timeout=30000)
+
+while not wlan.isconnected(): 
+     machine.idle()
+
+print("Connected to Wifi\n")
+client = MQTTClient(' ', 'test.mosquitto.org', port=8884, keepalive=30, ssl=True, ssl_params = PARAMS)
+client.settimeout = settimeout
+client.connect()
+
+while True:
+     print("Sending ON")
+     client.publish(topic="/lights", msg="ON")
+     time.sleep(5)
+     print("Sending OFF")
+     client.publish(topic="/lights", msg="OFF")
+     time.sleep(5)
